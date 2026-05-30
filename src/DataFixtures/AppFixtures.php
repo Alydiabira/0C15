58e5a8fda46c72cb9b884ca4dbcc64130843f306
Aly <d-aly@outlook.fr>
2026-05-30 19:23:35 +0200

    Mise à jour

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,9 +10,7 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function __construct(private UserPasswordHasherInterface $hasher)
-    {
-    }
+    public function __construct(private UserPasswordHasherInterface $hasher) {}
 
     public function load(ObjectManager $manager): void
     {
@@ -21,25 +19,38 @@ class AppFixtures extends Fixture
         $ina->setEmail('ina@test.com');
         $ina->setName('Ina');
         $ina->setType('ina');
-        $ina->setRoles(['ROLE_INA']); // ← indispensable
+        $ina->setRoles(['ROLE_INA']);
         $ina->setPassword($this->hasher->hashPassword($ina, 'password'));
         $manager->persist($ina);
+        $this->addReference('user_ina', $ina);
 
-        // INVITE (guest)
+        // INVITE (utilisateur invité)
         $invite = new User();
         $invite->setEmail('invite@test.com');
         $invite->setName('Invite');
         $invite->setType('invite');
-        $invite->setRoles(['ROLE_USER']); // ← indispensable
+        $invite->setRoles(['ROLE_USER']);
         $invite->setPassword($this->hasher->hashPassword($invite, 'password'));
         $manager->persist($invite);
+        $this->addReference('user_invite', $invite);
 
-        // MEDIA pour les tests
+        // GUEST (invité affiché côté front)
+        $guest = new User();
+        $guest->setEmail('guest@test.com');
+        $guest->setName('Guest');
+        $guest->setType('guest');
+        $guest->setRoles(['ROLE_USER']);
+        $guest->setPassword($this->hasher->hashPassword($guest, 'password'));
+        $manager->persist($guest);
+        $this->addReference('user_guest', $guest);
+
+        // MEDIA pour les tests (lié à l'invité)
         $media = new Media();
         $media->setTitle('Media test');
         $media->setPath('uploads/test.jpg');
         $media->setUser($invite);
         $manager->persist($media);
+        $this->addReference('media_test', $media);
 
         $manager->flush();
     }
--- tests/Functional/GuestShowTest.php
+++ tests/Functional/GuestShowTest.php
@@ -13,8 +13,11 @@ class GuestShowTest extends WebTestCase
         $client = static::createClient();
         $this->loginAsUser($client);
 
-        // ID 8 existe dans ta base de test
-        $client->request('GET', '/guest/8');
+        // Récupération du guest créé dans les fixtures
+        $guest = self::getContainer()->get('doctrine')->getRepository(\App\Entity\User::class)
+            ->findOneBy(['type' => 'guest']);
+
+        $client->request('GET', '/guest/' . $guest->getId());
 
         $this->assertResponseStatusCodeSame(200);
     }
